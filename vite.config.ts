775cf216295d4fa34b3df94dbe712c8dea9f40db
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2025-02-22 13:27:36 +0800

    Vite plug in removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { courseStructurePlugin } from './vite-plugin-course-structure.js'
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), courseStructurePlugin()]
+	plugins: [sveltekit(), tailwindcss()]
 });
